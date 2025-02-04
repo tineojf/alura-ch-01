17368bb567ddd9d9f8d64692dd7a5bc61cc75194
tineojf <92743762+tineodev@users.noreply.github.com>
2025-02-04 12:55:56 -0500

    feat: add function to randomly draw a secret friend from the list

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 const inputAmigo = document.querySelector('#amigo');
 const ulAmigos = document.querySelector('#listaAmigos');
+const btnSortear = document.querySelector('.button-draw');
+const ulResultado = document.querySelector('#resultado');
 
 let amigos = [];
 
@@ -31,3 +33,24 @@ function actualizarListaHTML(listaHTML, listaJS) {
 }
 
 // sortearAmigo();
+function sortearAmigo() {
+  const cantidadAmigos = amigos.length;
+  if (cantidadAmigos == 0) {
+    alert('Primero añade algún amigo para realizar el sorteo');
+    return
+  }
+
+  ulResultado.innerHTML = "";
+
+  const indice = numeroRandom(1, cantidadAmigos);
+  console.log(indice);
+
+  const li = document.createElement('li')
+  li.innerText = `El amigo secreto sorteado es: ${amigos[indice - 1]}`;
+
+  ulResultado.appendChild(li);
+}
+
+function numeroRandom(inicio, fin) {
+  return Math.floor(Math.random() * (fin - inicio + 1) + inicio)
+}
