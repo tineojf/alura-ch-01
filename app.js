24580a487108d266aebedfc5ff35b8792072bb0c
tineojf <92743762+tineodev@users.noreply.github.com>
2025-02-04 12:27:53 -0500

    feat: add function agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+const inputAmigo = document.querySelector('#amigo');
+const liAmigos = document.querySelector('#listaAmigos');
+
+let amigos = [];
+
+// agregarAmigo();
+function agregarAmigo() {
+  let value = inputAmigo.value;
+
+  if (value.trim() == "") {
+    alert("Por favor, inserte un nombre");
+    return;
+  }
+
+  amigos.push(value)
+  console.log(amigos);
+  inputAmigo.value = "";
+}
+
+
+// sortearAmigo();
